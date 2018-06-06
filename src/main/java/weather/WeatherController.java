8889f1faef9bd528d31bb4e17d25a63ce7c2585e
Rango-Cheriff <qm5zp2@gmail.com>
2018-06-06 22:28:33 +0300

    move owm api usage to WeatherService

--- src/main/java/weather/WeatherController.java
+++ src/main/java/weather/WeatherController.java
@@ -1,9 +1,7 @@
 package weather;
 
-import net.aksingh.owmjapis.api.APIException;
-import net.aksingh.owmjapis.core.OWM;
-import net.aksingh.owmjapis.model.CurrentWeather;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WeatherController {
     private static final Logger LOG = Logger.getLogger(WeatherController.class);
-    private static OWM owm = new OWM("07cb39d0d64307ac8862d0136a00bd0e");
+
+    @Autowired
+    WeatherService weatherService;
 
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public String search(
@@ -25,109 +25,109 @@ public class WeatherController {
 
         String weather;
 
+        LOG.info("SEARCH WITH : { id=" + cityId + ", cityName=" + cityName + ", lon=" + longitude + ", lat=" + latitude + "}");
+
         // ALL PARAMS CHECK
         if ((cityId == null || cityId.isEmpty()) && (cityName == null || cityName.isEmpty()) && ((latitude == null || latitude.isEmpty()) && (longitude == null || longitude.isEmpty()))) {
             String errorString = "All params are empty";
-            LOG.error(errorString, new RuntimeException());
+            LOG.warn(errorString);
             model.addAttribute("errorString", errorString);
+
             return "exceptionPage";
         }
 
         // CITY ID CHECK
         try {
             if (cityId != null && !cityId.isEmpty() && Integer.parseInt(cityId) > 0) {
-                weather = getWeatherForCityId(cityId);
+                weather = weatherService.getWeatherForCityId(cityId);
+                LOG.debug("Search for city id: " + cityId);
+                if (weather.isEmpty()) {
+                    String errorString = "Weather for cityId = " + cityId + " not found";
+                    model.addAttribute("errorString", errorString);
+                    LOG.warn(errorString);
+                    return "exceptionPage";
+                }
                 model.addAttribute("cityId", cityId);
                 model.addAttribute("weather", weather);
-                LOG.info("Search for city id: " + cityId);
+
                 return "search";
-            } else {
-                LOG.info("City id is empty");
             }
         } catch (NumberFormatException e) {
             String errorString = "invalid param cityId = " + cityId;
-            LOG.error(errorString);
+            LOG.warn(errorString);
             model.addAttribute("errorString", errorString);
+            return "exceptionPage";
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            model.addAttribute("errorString", e.getMessage());
             return "exceptionPage";
         }
 
         // CITY NAME CHECK
-        if (cityName != null && !cityName.isEmpty()) {
-            weather = getWeatherForCityName(cityName);
-            if (weather.isEmpty()) {
-                String errorString = "Weather for cityName = " + cityName + " not found";
-                model.addAttribute("errorString", errorString);
-                LOG.warn(errorString);
-                return "exceptionPage";
+        try {
+            if (cityName != null && !cityName.isEmpty()) {
+                weather = weatherService.getWeatherForCityName(cityName);
+                LOG.debug("Search for city name: " + cityName);
+                if (weather.isEmpty()) {
+                    String errorString = "Weather for cityName = " + cityName + " not found";
+                    model.addAttribute("errorString", errorString);
+                    LOG.warn(errorString);
+                    return "exceptionPage";
+                }
+                model.addAttribute("cityName", cityName);
+                model.addAttribute("weather", weather);
+
+                return "search";
             }
-            model.addAttribute("cityName", cityName);
-            model.addAttribute("weather", weather);
-            LOG.info("Search for city name: " + cityName);
-            return "search";
-        } else {
-            String errorString = "City name is empty";
-            LOG.info(errorString);
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            model.addAttribute("errorString", e.getMessage());
+            return "exceptionPage";
         }
 
         // COORDS CHECK
         try {
             if (latitude != null && !latitude.isEmpty() && longitude != null && !longitude.isEmpty() && Double.parseDouble(latitude) > 0 && Double.parseDouble(longitude) > 0) {
-                weather = getWeatherForCoordinates(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                weather = weatherService.getWeatherForCoordinates(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                LOG.debug("Search for coords: lat=" + latitude + ", lon=" + longitude);
+                if (weather.isEmpty()) {
+                    String errorString = "Weather for coords: lon=" + longitude + " & lat=" + latitude + " not found";
+                    model.addAttribute("errorString", errorString);
+                    LOG.warn(errorString);
+                    return "exceptionPage";
+                }
                 model.addAttribute("latitude", latitude);
                 model.addAttribute("longitude", longitude);
                 model.addAttribute("weather", weather);
-                LOG.info("Search for coords: lat=" + latitude + ", lon=" + longitude);
+
                 return "search";
-            } else {
-                LOG.info("latitude & longitude are empty");
             }
         } catch (NumberFormatException e) {
             String errorString = "invalid params for lat= " + latitude + ", lon=" + longitude;
-            LOG.error(errorString);
+            LOG.warn(errorString);
             model.addAttribute("errorString", errorString);
+            return "exceptionPage";
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            model.addAttribute("errorString", e.getMessage());
             return "exceptionPage";
         }
 
         // DEFAULT
-        weather = "NOT FOUND. Default: " + getWeatherForCityId("498817");
-        model.addAttribute("weather", weather);
-        LOG.info("DEFAULT search");
-        return "search";
-    }
-
-    public static String getWeatherForCityId(String cityId) {
         try {
-            owm.setUnit(OWM.Unit.METRIC);
-            CurrentWeather currentWeather = owm.currentWeatherByCityId(Integer.parseInt(cityId));
+            weather = "NOT FOUND. Default: " + weatherService.getWeatherForCityId("498817");
+            LOG.debug("DEFAULT search");
+            model.addAttribute("cityId", cityId);
+            model.addAttribute("cityName", cityName);
+            model.addAttribute("latitude", latitude);
+            model.addAttribute("longitude", longitude);
+            model.addAttribute("weather", weather);
 
-            return "WEATHER FOR CITY ID(" + cityId + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now";
-        } catch (APIException e) {
-            LOG.error(e);
+            return "search";
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            model.addAttribute("errorString", e.getMessage());
+            return "exceptionPage";
         }
-        return "";
-    }
-
-    public static String getWeatherForCityName(String cityName) {
-        try {
-            owm.setUnit(OWM.Unit.METRIC);
-            CurrentWeather currentWeather = owm.currentWeatherByCityName(cityName);
-
-            return "WEATHER FOR CITY NAME(" + cityName + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now";
-        } catch (APIException e) {
-            LOG.error(e);
-        }
-        return "";
-    }
-
-    public static String getWeatherForCoordinates(double latitude, double longitude) {
-        try {
-            owm.setUnit(OWM.Unit.METRIC);
-            CurrentWeather currentWeather = owm.currentWeatherByCoords(latitude, longitude);
-
-            return "WEATHER FOR CITY COORDINATES(lat: " + latitude + ", lon: " + longitude + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now";
-        } catch (APIException e) {
-            LOG.error(e);
-        }
-        return "";
     }
 }
