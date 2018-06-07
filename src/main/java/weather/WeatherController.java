35597fece9637e72dd6105f9c0a3d765db68b958
Rango-Cheriff <qm5zp2@gmail.com>
2018-06-07 23:53:00 +0300

    fix errors, add notification

--- src/main/java/weather/WeatherController.java
+++ src/main/java/weather/WeatherController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.concurrent.Future;
-
 @Controller
 public class WeatherController {
     private static final Logger LOG = Logger.getLogger(WeatherController.class);
@@ -19,15 +17,28 @@ public class WeatherController {
 
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public String search(
+            @RequestParam(value = "userName", required = false, defaultValue = "anonymous") String userName,
             @RequestParam(value = "cityId", required = false, defaultValue = "") String cityId,
             @RequestParam(value = "cityName", required = false, defaultValue = "") String cityName,
             @RequestParam(value = "latitude", required = false, defaultValue = "") String latitude,
             @RequestParam(value = "longitude", required = false, defaultValue = "") String longitude,
             Model model) {
 
-        Future<String> weather;
+        String weather;
 
-        LOG.info("SEARCH WITH : { id=" + cityId + ", cityName=" + cityName + ", lon=" + longitude + ", lat=" + latitude + "}");
+        LOG.info("USER: " + userName + " SEARCH WITH: { id=" + cityId + ", cityName=" + cityName + ", lon=" + longitude + ", lat=" + latitude + "}");
+
+        if (userName == null || userName.isEmpty()) {
+            String errorString = "NOT AUTHORIZED";
+            LOG.warn(errorString);
+            model.addAttribute("errorString", errorString);
+            return "exceptionPage";
+        } else if (userName.equals("anonymous")) {
+            String errorString = "anonymous users are not allowed";
+            LOG.warn(errorString);
+            model.addAttribute("errorString", errorString);
+            return "exceptionPage";
+        }
 
         // ALL PARAMS CHECK
         if ((cityId == null || cityId.isEmpty()) && (cityName == null || cityName.isEmpty()) && ((latitude == null || latitude.isEmpty()) && (longitude == null || longitude.isEmpty()))) {
@@ -43,18 +54,15 @@ public class WeatherController {
             if (cityId != null && !cityId.isEmpty() && Integer.parseInt(cityId) > 0) {
                 weather = weatherService.getWeatherForCityId(cityId);
                 LOG.debug("Search for city id: " + cityId);
-                while (!weather.isDone()) {
-                    Thread.sleep(10);
-                }
-                if (weather.get().isEmpty()) {
+                if (weather == null || weather.isEmpty()) {
                     String errorString = "Weather for cityId = " + cityId + " not found";
                     model.addAttribute("errorString", errorString);
                     LOG.warn(errorString);
                     return "exceptionPage";
                 }
                 model.addAttribute("cityId", cityId);
-                model.addAttribute("weather", weather.get());
-
+                model.addAttribute("weather", weather);
+                LOG.info("USER: " + userName + " GET WEATHER: " + weather);
                 return "search";
             }
         } catch (NumberFormatException e) {
@@ -73,18 +81,15 @@ public class WeatherController {
             if (cityName != null && !cityName.isEmpty()) {
                 weather = weatherService.getWeatherForCityName(cityName);
                 LOG.debug("Search for city name: " + cityName);
-                while (!weather.isDone()) {
-                    Thread.sleep(10);
-                }
-                if (weather.get().isEmpty()) {
+                if (weather == null || weather.isEmpty()) {
                     String errorString = "Weather for cityName = " + cityName + " not found";
                     model.addAttribute("errorString", errorString);
                     LOG.warn(errorString);
                     return "exceptionPage";
                 }
                 model.addAttribute("cityName", cityName);
-                model.addAttribute("weather", weather.get());
-
+                model.addAttribute("weather", weather);
+                LOG.info("USER: " + userName + " GET WEATHER: " + weather);
                 return "search";
             }
         } catch (Exception e) {
@@ -95,13 +100,16 @@ public class WeatherController {
 
         // COORDS CHECK
         try {
-            if (latitude != null && !latitude.isEmpty() && longitude != null && !longitude.isEmpty() && Double.parseDouble(latitude) > 0 && Double.parseDouble(longitude) > 0) {
+            if (latitude != null && !latitude.isEmpty() && longitude != null && !longitude.isEmpty()) {
+                if (Double.parseDouble(latitude) < -90 || Double.parseDouble(latitude) > 90 || Double.parseDouble(longitude) < -180 || Double.parseDouble(longitude) > 180) {
+                    String errorString = "Bad coords: lon=" + longitude + "(min -180, max 180) & lat=" + latitude + "(min -90, max 90)";
+                    model.addAttribute("errorString", errorString);
+                    LOG.warn(errorString);
+                    return "exceptionPage";
+                }
                 weather = weatherService.getWeatherForCoordinates(Double.parseDouble(latitude), Double.parseDouble(longitude));
                 LOG.debug("Search for coords: lat=" + latitude + ", lon=" + longitude);
-                while (!weather.isDone()) {
-                    Thread.sleep(10);
-                }
-                if (weather.get().isEmpty()) {
+                if (weather == null || weather.isEmpty()) {
                     String errorString = "Weather for coords: lon=" + longitude + " & lat=" + latitude + " not found";
                     model.addAttribute("errorString", errorString);
                     LOG.warn(errorString);
@@ -109,9 +117,11 @@ public class WeatherController {
                 }
                 model.addAttribute("latitude", latitude);
                 model.addAttribute("longitude", longitude);
-                model.addAttribute("weather", weather.get());
-
+                model.addAttribute("weather", weather);
+                LOG.info("USER: " + userName + " GET WEATHER: " + weather);
                 return "search";
+            } else if ((latitude == null || latitude.isEmpty()) && (longitude != null && !longitude.isEmpty()) || ((longitude == null || longitude.isEmpty()) && (latitude != null && !latitude.isEmpty()))) {
+                throw new NumberFormatException();
             }
         } catch (NumberFormatException e) {
             String errorString = "invalid params for lat= " + latitude + ", lon=" + longitude;
@@ -128,15 +138,18 @@ public class WeatherController {
         try {
             weather = weatherService.getDefaultWeather();
             LOG.debug("DEFAULT search");
-            while (!weather.isDone()) {
-                Thread.sleep(10);
+            if (weather == null || weather.isEmpty()) {
+                String errorString = "Defaul weather not found";
+                model.addAttribute("errorString", errorString);
+                LOG.error(errorString);
+                return "exceptionPage";
             }
             model.addAttribute("cityId", cityId);
             model.addAttribute("cityName", cityName);
             model.addAttribute("latitude", latitude);
             model.addAttribute("longitude", longitude);
-            model.addAttribute("weather", weather.get());
-
+            model.addAttribute("weather", weather);
+            LOG.info("USER: " + userName + " GET WEATHER: " + weather);
             return "search";
         } catch (Exception e) {
             LOG.error(e.getMessage());
--- src/main/java/weather/WeatherService.java
+++ src/main/java/weather/WeatherService.java
@@ -1,50 +1,92 @@
 package weather;
 
-import net.aksingh.owmjapis.api.APIException;
 import net.aksingh.owmjapis.core.OWM;
 import net.aksingh.owmjapis.model.CurrentWeather;
 import org.apache.log4j.Logger;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 @Service
 public class WeatherService {
     private static final Logger LOG = Logger.getLogger(WeatherController.class);
     private static OWM owm = new OWM("07cb39d0d64307ac8862d0136a00bd0e");
 
-    @Async
-    public Future<String> getWeatherForCityId(String cityId) throws APIException, InterruptedException {
-        owm.setUnit(OWM.Unit.METRIC);
-        CurrentWeather currentWeather = owm.currentWeatherByCityId(Integer.parseInt(cityId));
-        Thread.sleep(new Random().nextInt(5000));
-        return new AsyncResult<String>("WEATHER FOR CITY ID(" + cityId + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+    private static ExecutorService service = Executors.newFixedThreadPool(1);
+
+    public String getWeatherForCityId(String cityId) throws InterruptedException {
+        CurrentWeather currentWeather = getWeather(cityId, null, 0, 0);
+        if (currentWeather != null) {
+            return new String("WEATHER FOR CITY ID (" + cityId + "): \n" + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+        } else {
+            return null;
+        }
     }
 
-    @Async
-    public Future<String> getWeatherForCityName(String cityName) throws APIException, InterruptedException {
-        owm.setUnit(OWM.Unit.METRIC);
-        CurrentWeather currentWeather = owm.currentWeatherByCityName(cityName);
-        Thread.sleep(new Random().nextInt(5000));
-        return new AsyncResult<String>("WEATHER FOR CITY NAME(" + cityName + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+    public String getWeatherForCityName(String cityName) throws InterruptedException {
+        CurrentWeather currentWeather = getWeather(null, cityName, 0, 0);
+        if (currentWeather != null) {
+            return new String("WEATHER FOR CITY NAME (" + cityName + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+        } else {
+            return null;
+        }
     }
 
-    @Async
-    public Future<String> getWeatherForCoordinates(double latitude, double longitude) throws APIException, InterruptedException {
-        owm.setUnit(OWM.Unit.METRIC);
-        CurrentWeather currentWeather = owm.currentWeatherByCoords(latitude, longitude);
-        Thread.sleep(new Random().nextInt(5000));
-        return new AsyncResult<>("WEATHER FOR COORDINATES(lat: " + latitude + ", lon: " + longitude + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+    public String getWeatherForCoordinates(double latitude, double longitude) throws InterruptedException {
+        CurrentWeather currentWeather = getWeather(null, null, latitude, longitude);
+        if (currentWeather != null) {
+            return new String("WEATHER FOR COORDINATES (lat: " + latitude + ", lon: " + longitude + "): " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+        } else {
+            return null;
+        }
     }
 
-    @Async
-    public Future<String> getDefaultWeather() throws APIException, InterruptedException {
+    public String getDefaultWeather() throws InterruptedException {
+        CurrentWeather currentWeather = getWeather("498817", null, 0, 0);
+        if (currentWeather != null) {
+            return new String("NOT FOUND. Default: " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+        } else {
+            return null;
+        }
+    }
+
+    private CurrentWeather getWeather(String cityId, String cityName, double latitude, double longitude) throws InterruptedException {
         owm.setUnit(OWM.Unit.METRIC);
-        CurrentWeather currentWeather = owm.currentWeatherByCityId(498817);
-        Thread.sleep(new Random().nextInt(5000));
-        return new AsyncResult<String>("NOT FOUND. Default: " + currentWeather.getMainData().getTemp() + "°C " + currentWeather.getWeatherList().get(0).getDescription() + " at " + currentWeather.getCityName() + " now");
+
+        Callable<CurrentWeather> callable = () -> {
+            CurrentWeather weather = null;
+
+            try {
+                if (cityId != null && !cityId.isEmpty()) {
+                    Thread.sleep(new Random().nextInt(5000));
+                    return owm.currentWeatherByCityId(Integer.parseInt(cityId));
+                } else if (cityName != null && !cityName.isEmpty()) {
+                    Thread.sleep(new Random().nextInt(5000));
+                    return owm.currentWeatherByCityName(cityName);
+                } else if (latitude < 90 && latitude > -90 && longitude < 180 && longitude > -180) {
+                    return owm.currentWeatherByCoords(latitude, longitude);
+                } else {
+                    throw new RuntimeException("Not valid input");
+                }
+            } catch (Exception e) {
+                LOG.error(e.getMessage());
+            }
+
+            return weather;
+        };
+
+        Future<CurrentWeather> future = service.submit(callable);
+
+        while (!future.isDone() && !future.isCancelled()) {
+            Thread.sleep(500);
+        }
+
+        try {
+            return future.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
